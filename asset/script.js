ce21bc76c599ee19ed121ed33f85ce63f43514da
MasEBTA <ebta.sb@gmail.com>
2023-04-12 15:05:08 +0700

    almost done

--- asset/script.js
+++ asset/script.js
@@ -48,3 +48,63 @@ function logicTest() {
   $('.modal-title').text('Analisa Semantik HTML');
   $('object').attr('data', 'test-logic/index.html');
 }
+function luasBangunDatar() {
+  openModal();
+  $('.modal-title').text('Luas Bangun Datar');
+  $('object').attr('data', 'bangun-datar-luas/index.html');
+}
+function frontendSkill() {
+  openModal();
+  $('.modal-title').text('Front End');
+  $('object').attr('data', 'sertif/frontend/index.html');
+}
+function htmlSkill() {
+  openModal();
+  $('.modal-title').html('Html Sertificate');
+  $('object').attr('data', 'sertif/html/index.html');
+}
+function cssSkill() {
+  openModal();
+  $('.modal-title').text('Css Sertificate');
+  $('object').attr('data', 'sertif/css/index.html');
+}
+function jsSkill() {
+  openModal();
+  $('.modal-title').text('Javascript Sertificate');
+  $('object').attr('data', 'sertif/js/index.html');
+}
+function bootstrapSkill() {
+  openModal();
+  $('.modal-title').text('Bootstrap Sertificate');
+  $('object').attr('data', 'sertif/bootstrap/index.html');
+}
+function phpSkill() {
+  openModal();
+  $('.modal-title').text('Php Sertificate');
+  $('object').attr('data', 'sertif/php/index.html');
+}
+function laravelSkill() {
+  openModal();
+  $('.modal-title').text('Laravel Sertificate');
+  $('object').attr('data', 'sertif/laravel/index.html');
+}
+function vueSkill() {
+  openModal();
+  $('.modal-title').text('Vue Js Sertificate');
+  $('object').attr('data', 'sertif/vue/index.html');
+}
+function jquerySkill() {
+  openModal();
+  $('.modal-title').text('Jquery Sertificate');
+  $('object').attr('data', 'sertif/jquery/index.html');
+}
+function githubSkill() {
+  openModal();
+  $('.modal-title').text('Git & Github Sertificate');
+  $('object').attr('data', 'sertif/github/index.html');
+}
+function sqlSkill() {
+  openModal();
+  $('.modal-title').text('Sql Sertificate');
+  $('object').attr('data', 'sertif/sql/index.html');
+}
